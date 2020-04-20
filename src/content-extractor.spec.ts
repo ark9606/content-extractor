d4fdc84f13cff1e411c0edcaaa03c744ccad4501
Arkady Zelensky <arkady.zelensky@yalantis.com>
2020-04-20 16:47:01 +0300

    initial commit

---
+++ src/content-extractor.spec.ts
@@ -0,0 +1,9 @@
+import { ContentExtractor } from './content-extractor';
+
+describe('ContentExtractor', () => {
+  const e = new ContentExtractor();
+
+  it('should be defined', () => {
+    expect(e).toBeDefined();
+  });
+});
